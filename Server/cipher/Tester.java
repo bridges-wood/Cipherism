63d1dbacb46944fe2a0bd3af37c8907703e07294
woodmb <woodmb@IT-05.ksc.internal>
2019-06-19 14:42:56 +0100

    Changes to detect english.

--- Server/cipher/Tester.java
+++ Server/cipher/Tester.java
@@ -1,4 +1,5 @@
 package cipher;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -7,10 +8,11 @@ public class Tester {
 	Tester() {
 		run();
 	}
-	
+
 	public void run() {
 		String otherText = "thisisverysimilartoenglishtext";
-		//String otherOtherText = "King’sisaveryspecialplace,themomentyouwalkthroughthedoorsyoucansensethewarmth,purposeandenergyofallthepupilsandstaff.Thisisaschoolwhichvaluespositiverelationships,opportunityandgreatlearningaboveallelse,therebyallowingourpupilstomaketheverymostofthemselvesataschooltheylove.";
+		// String otherOtherText =
+		// "King’sisaveryspecialplace,themomentyouwalkthroughthedoorsyoucansensethewarmth,purposeandenergyofallthepupilsandstaff.Thisisaschoolwhichvaluespositiverelationships,opportunityandgreatlearningaboveallelse,therebyallowingourpupilstomaketheverymostofthemselvesataschooltheylove.";
 		// for (String word : PredictWords.predictedWords("complexity", true)) { // Test
 		// single word prediction.
 		// System.out.println(word);
@@ -23,15 +25,17 @@ public class Tester {
 		// otherText).entrySet()) {
 		// System.out.println(entry.getKey() + ":" + entry.getValue());
 		// }
-		//int[] keys = KasiskiExamination.likelyKeyLengths(NGramAnalyser.kasiskiBase(3, otherText), otherText);
-		//System.out.println(KasiskiExamination.keyGuesserVigenere(KasiskiExamination.mostLikelyKeyLength(otherText, keys), otherText));
+		// int[] keys = KasiskiExamination.likelyKeyLengths(NGramAnalyser.kasiskiBase(3,
+		// otherText), otherText);
+		// System.out.println(KasiskiExamination.keyGuesserVigenere(KasiskiExamination.mostLikelyKeyLength(otherText,
+		// keys), otherText));
 		// System.out.println(DetectEnglish.detectEnglish(otherText));
 		// Utilities.generateObjectHashTable("2grams.txt ", "2grams.ohtb");
 		// System.out.println(Utilities.readHashTable("hashed_dictionary.htb").keySet().contains(Utilities.hash64("ghgashdfj")));
-		DetectEnglish a = new DetectEnglish();
+		DetectEnglish b = new DetectEnglish();
 		// System.out.println(a.respace(otherOtherText));
-		//System.out.println(NGramAnalyser.computeScore(3, otherText, false));
-		a.score(otherText, 20);
+		// System.out.println(NGramAnalyser.computeScore(3, otherText, false));
+		System.out.println(b.viterbiRespace(otherText,20));
 	}
 
 	public static void main(String[] args) {
