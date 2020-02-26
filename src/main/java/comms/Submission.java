4bc20e6343ea1eb4410e766323bf792060300a6c
Max Wood <bridges.wood@gmail.com>
2020-02-26 21:00:34 +0000

    Changes to web connection architecture

---
+++ src/main/java/comms/Submission.java
@@ -0,0 +1,24 @@
+package comms;
+
+public class Submission {
+
+	private String cipherText;
+	private String plainText;
+
+	public String getCipherText() {
+		return cipherText;
+	}
+
+	public void setCipherText(String cipherText) {
+		this.cipherText = cipherText;
+	}
+
+	public String getPlainText() {
+		return plainText;
+	}
+
+	public void setPlainText(String plainText) {
+		this.plainText = plainText;
+	}
+
+}
