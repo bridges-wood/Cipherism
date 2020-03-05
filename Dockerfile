b8657e35fe2167252462c49d3de79160b6d9f1f1
Max Wood <bridges.wood@gmail.com>
2020-03-05 18:51:34 +0000

    Structural changes to image

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 COPY src/main/resources/* /src/main/resources/
 ENTRYPOINT ["java","-jar","/app.jar"]
-
+EXPOSE 8100
--- src/main/java/com/polytonic/comms/Server.java
+++ src/main/java/com/polytonic/comms/Server.java
@@ -2,11 +2,27 @@ package com.polytonic.comms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.polytonic.cipher.Manager;
+
+@RestController
 @SpringBootApplication
 public class Server {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Server.class, args);
+	}
+	
+	@GetMapping("/ciphertext/{cipherText}")
+	public String decodeCipherText(@PathVariable String cipherText) {
+		cipherText.replaceAll("%20", " ");
+		Manager m = new Manager(cipherText, false);
+		Gson g = new Gson();
+		APIResponse responseObj = new APIResponse(m.getResult(), m.isFail());
+		return g.toJson(responseObj);
 	}
 }
