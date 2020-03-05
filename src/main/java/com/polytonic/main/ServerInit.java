31543381bb6eb608206d1edcc430cb943b8c88ca
Max Wood <bridges.wood@gmail.com>
2020-03-05 07:44:39 +0000

    Debug string

--- src/main/java/com/polytonic/main/ServerInit.java
+++ src/main/java/com/polytonic/main/ServerInit.java
@@ -1,7 +1,5 @@
 package com.polytonic.main;
 
-import com.polytonic.cipher.InitialiseStaticResources;
-
 /**
  * Class that manages the initialisation of all resources necessary for the
  * operation of the server.
@@ -11,7 +9,8 @@ import com.polytonic.cipher.InitialiseStaticResources;
 public class ServerInit {
 
 	public static void main(String[] args) {
-		//InitialiseStaticResources.uInit(true, true, true, true, true);
+		// InitialiseStaticResources.uInit(true, true, true, true, true);
+		System.out.println("Initialising server.");
 		com.polytonic.comms.Server.main(new String[0]);
 	}
 
