3705d5beddd4fa11a5bd23af5eafe6728febc253
Max Wood <bridges.wood@gmail.com>
2020-03-05 20:03:32 +0000

    Changes to server configuration

--- src/main/java/com/polytonic/comms/Server.java
+++ src/main/java/com/polytonic/comms/Server.java
@@ -16,10 +16,11 @@ public class Server {
 	public static void main(String[] args) {
 		SpringApplication.run(Server.class, args);
 	}
-	
+
 	@GetMapping("/ciphertext/{cipherText}")
 	public String decodeCipherText(@PathVariable String cipherText) {
 		cipherText.replaceAll("%20", " ");
+		System.out.println("Manager started on text: " + cipherText);
 		Manager m = new Manager(cipherText, false);
 		Gson g = new Gson();
 		APIResponse responseObj = new APIResponse(m.getResult(), m.isFail());
