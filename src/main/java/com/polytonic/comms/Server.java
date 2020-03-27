9a44e1cf68b8e2217ed85d9d7ca762e4f23cdbc2
Max Wood <bridges.wood@gmail.com>
2020-03-27 10:57:37 +0000

    Changed parameter acquisition away from path to request.

--- src/main/java/com/polytonic/comms/Server.java
+++ src/main/java/com/polytonic/comms/Server.java
@@ -3,7 +3,7 @@ package com.polytonic.comms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -17,11 +17,11 @@ public class Server {
 		SpringApplication.run(Server.class, args);
 	}
 
-	@GetMapping("/ciphertext/{cipherText}")
-	public String decodeCipherText(@PathVariable String cipherText) {
-		cipherText.replaceAll("%20", " ");
-		System.out.println("Manager started on text: " + cipherText);
-		Manager m = new Manager(cipherText, false, false);
+	@GetMapping("/")
+	public String decodeCipherText(@RequestParam String cipher) {
+		cipher.replaceAll("%20", " ");
+		System.out.println("Manager started on text: " + cipher);
+		Manager m = new Manager(cipher, false, false);
 		Gson g = new Gson();
 		APIResponse responseObj = new APIResponse(m.getResult(), m.isFail());
 		return g.toJson(responseObj);
